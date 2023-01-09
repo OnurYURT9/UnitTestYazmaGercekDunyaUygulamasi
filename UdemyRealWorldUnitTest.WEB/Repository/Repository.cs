4c099e99d6dbe3da2a28c320e42df905ed64a948
ONUR YURT <oyurt43@gmail.com>
2023-01-10 00:00:48 +0300

    ilk

---
+++ UdemyRealWorldUnitTest.WEB/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+using UdemyRealWorldUnitTest.WEB.Models;
+using UdemyRealWorldUnitTest.WEB.Repository;
+namespace UdemyRealWorldUnitTest.WEB.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly UdemyunitTestDBContext _context;
+        private readonly DbSet<TEntity> _dbset;
+        public Repository(UdemyunitTestDBContext context)
+        {
+            _context = context;
+            _dbset= _context.Set<TEntity>();
+        }
+        public async Task Create(TEntity entity)
+        {
+            await _dbset.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbset.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbset.FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+           //_dbset.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
